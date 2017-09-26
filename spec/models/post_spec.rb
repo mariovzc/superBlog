538640e6d76f8a5e4ea6a429adc846cbfb06e10b
¨Mario <mario.vizcainos@gmail.com>
2017-09-26 14:09:50 -0500

    created model test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should validate_presence_of(:title) }
+
+    it { should validate_presence_of(:body) }
+  end
+
+  describe "Associations" do
+    it { should belong_to(:user) }
+  end
 end
