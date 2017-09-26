bdd0c7ee8a083e52a660f25527464ed3d246338c
¨Mario <mario.vizcainos@gmail.com>
2017-09-26 11:25:26 -0500

    added model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  #Validations
+  validates :password,
+            length: { minimum: 6 , message: 'is too short (minimum is 6 characters)'},
+            confirmation: {message: 'You must confirm your password'}
+  
+  validates :username,
+            uniqueness: {message: "has already been taken"} ,
+            length: { minimum: 6, message: 'is too short (minimum is 6 characters)' },case_sensitive: false
+  
+  validates :email,
+            uniqueness: {message: "has already been taken"},
+            presence: { message: "can't be blank"},case_sensitive: false
+
+  validates :username,
+            :last_name,
+            :first_name ,
+            presence: { message: "can't be blank"}
+  
 end
