3bfaa19f202129a8464aea161c613e5682644dae
¨Mario <mario.vizcainos@gmail.com>
2017-09-26 11:48:47 -0500

    created sessions controllers view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
   resources :users, only: [:new, :create]
   get '/sign_up', to: 'users#new', as: :sign_up  
+
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/log_in', to: 'sessions#new', as: :log_in
+  delete '/log_out', to: 'sessions#destroy', as: :log_out
+
 end
