e2538e144dfd9cd907ccefa71678181cb8111e70
¨Mario <mario.vizcainos@gmail.com>
2017-09-26 11:25:36 -0500

    created user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should validate_presence_of(:email) }
+
+    it { should validate_presence_of(:username) }
+
+    it { should validate_presence_of(:first_name) }
+
+    it { should validate_presence_of(:last_name) }
+
+    it do
+      should validate_length_of(:password).
+        is_at_least(6).
+        on(:create)
+    end
+  end
 end
