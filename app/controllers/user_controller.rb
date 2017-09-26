3b5baed588f629b8adc9ee5451bea5916a6038ad
¨Mario <mario.vizcainos@gmail.com>
2017-09-26 11:44:10 -0500

    execute login when the user create the a account

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login(params[:user][:email], params[:user][:password])
       flash[:success] = 'Welcome!'
       redirect_to root_path
     else
