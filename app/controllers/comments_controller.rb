55716b6df114cb793c19d7cabae35f2d15fbb1b3
¨Mario <mario.vizcainos@gmail.com>
2017-09-27 11:23:53 -0500

    comment methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    end
+  end
+  def destroy
+    @comment = Coment.find(params[:id])
+    if @comment.destroy
+      redirect_to :back
+    end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:body)    
+  end
 end
