8bd0ad05486b6e765381a61d957adea000656b16
¨Mario <mario.vizcainos@gmail.com>
2017-09-26 22:31:53 -0500

    added posts methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,32 @@ class PostsController < ApplicationController
   skip_before_action :require_login, only: [:index]
   
   def index
-    @post = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 5)    
   end
 
   def new
+  end
+
+  def create
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+    
+  end
+  
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    
   end
 end
