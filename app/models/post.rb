11674dcfabc00bd195669bd291217c598e56b9b6
¨Mario <mario.vizcainos@gmail.com>
2017-09-28 17:42:02 -0500

    fixed post form style

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   validates :title,
   :body,
   presence: { message: "can't be blank"}
-
+  validates :body, length: { minimum: 6 , message: 'is too short (minimum is 6 characters)'}
 end
